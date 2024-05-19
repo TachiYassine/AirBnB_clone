c4996c53e4b76c922100280638392fa5c89e9d1c
TachiYassine <tachi.yassine.3@gmail.com>
2024-05-18 22:36:56 -0700

    making the back bone -update

--- models/base_model.py
+++ models/base_model.py
@@ -8,13 +8,9 @@ serialization and deserialization, and will
 have other methods that will be explained later.
 """
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 from models import storage
-import uuid
-import json
-import sys
-import os.path
 
 
 class BaseModel:
@@ -30,16 +26,18 @@ class BaseModel:
         This is the constructor method for the BaseModel class,
         it initializes the instance variables and won't return any value.
         """
-        if kwargs:
-            dtf = '%Y-%m-%dT%H:%M:%S.%f'
-            kwargs_copy = kwargs.copy()
-            del kwargs_copy["__class__"]
-            for key in kwargs_copy:
-                if ("created_at" == key or "updated_at" == key):
-                    kwargs_copy[key] = datetime.strptime(kwargs_copy[key], dtf)
-            self.__dict__ = kwargs_copy
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
@@ -49,10 +47,8 @@ class BaseModel:
         This method returns a string representation of the instance:
         should print: [<class name>] (<self.id>) <self.__dict__>
         """
-        return ('[{}] ({}) {}'.format(
-            self.__class__.__name__,
-            self.id,
-            self.__class__.__dict__))
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self) -> None:
         """
@@ -67,24 +63,8 @@ class BaseModel:
         This method returns a dictionary containing all keys/values
         of __dict__ of the instance.
         """
-        result_dict = {}
-        result_dict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if isinstance(value, (datetime,)):
-                result_dict[key] = value.isoformat()
-            else:
-                result_dict[key] = value
-        return result_dict
-
-    def to_json(self):
-        """
-        Returns a JSON serializable dictionary containing all key-value pairs
-        of __dict__ of the instance.
-        """
-        json_dict = self.__dict__.copy()
-        dt_format = '%Y-%m-%dT%H:%M:%S.%f'
-        json_dict['created_at'] = self.created_at.strftime(dt_format)
-        json_dict['__class__'] = str(self.__class__.__name__)
-        if hasattr(self, 'updated_at'):
-            json_dict['updated_at'] = self.updated_at.strftime(dt_format)
-        return json_dict
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
