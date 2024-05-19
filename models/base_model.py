b6153f7297c7148011c38ff769420b892535e8b8
TachiYassine <tachi.yassine.3@gmail.com>
2024-05-18 21:18:29 -0700

    making the back bone -update

--- models/base_model.py
+++ models/base_model.py
@@ -30,25 +30,61 @@ class BaseModel:
         This is the constructor method for the BaseModel class,
         it initializes the instance variables and won't return any value.
         """
-        pass
+        if kwargs:
+            dtf = '%Y-%m-%dT%H:%M:%S.%f'
+            kwargs_copy = kwargs.copy()
+            del kwargs_copy["__class__"]
+            for key in kwargs_copy:
+                if ("created_at" == key or "updated_at" == key):
+                    kwargs_copy[key] = datetime.strptime(kwargs_copy[key], dtf)
+            self.__dict__ = kwargs_copy
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self) -> str:
         """
         This method returns a string representation of the instance:
         should print: [<class name>] (<self.id>) <self.__dict__>
         """
-        pass
+        return ('[{}] ({}) {}'.format(
+            self.__class__.__name__,
+            self.id,
+            self.__class__.__dict__))
 
     def save(self) -> None:
         """
         This method updates the public instance attribute updated_at
         with the current datetime and saves the instance.
         """
-        pass
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self) -> dict:
         """
         This method returns a dictionary containing all keys/values
         of __dict__ of the instance.
         """
-        pass
+        result_dict = {}
+        result_dict["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if isinstance(value, (datetime,)):
+                result_dict[key] = value.isoformat()
+            else:
+                result_dict[key] = value
+        return result_dict
+
+    def to_json(self):
+        """
+        Returns a JSON serializable dictionary containing all key-value pairs
+        of __dict__ of the instance.
+        """
+        json_dict = self.__dict__.copy()
+        dt_format = '%Y-%m-%dT%H:%M:%S.%f'
+        json_dict['created_at'] = self.created_at.strftime(dt_format)
+        json_dict['__class__'] = str(self.__class__.__name__)
+        if hasattr(self, 'updated_at'):
+            json_dict['updated_at'] = self.updated_at.strftime(dt_format)
+        return json_dict
