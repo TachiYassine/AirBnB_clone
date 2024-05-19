94c51c4a13ebe49c0bab7f98c556c2ae090943f5
TachiYassine <tachi.yassine.3@gmail.com>
2024-05-18 18:44:02 -0700

    Update for pycodestyle

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 """
-This file will be used to initialize our storage system for 
-the airbnb clone  project (for storing and managing data persistently). 
-The FileStorage class is instantiated, and we wll be using its reload method 
-to ensure that any existing data used in this project is loaded into memory when the application starts.
+This file will be used to initialize our storage system for
+the airbnb clone  project (for storing and managing data persistently).
+The FileStorage class is instantiated, and we wll be using its reload method
+to ensure that any existing data used in this project is loaded into memory
+when the application starts.
 """
 
 from .engine.file_storage import FileStorage
