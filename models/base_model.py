2712ab1adb3cd9474f20ab1f9d3e8f5cc73ac0c5
TachiYassine <tachi.yassine.3@gmail.com>
2024-05-18 18:26:33 -0700

    making the back bones -update for pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,54 @@
 #!/usr/bin/python3
 """
-The class BaseModel will be the back bone of our project 
-and will have all the common aspects (attributes, methods) 
-that the other classes share and will also be responsable for
-the initialization and conversion (JSON <=> Python): mainely called
-serialization and deserialization and will 
-have other methods that will be explained later
-
+The class BaseModel will be the backbone of our project
+and will have all the common aspects (attributes, methods)
+that the other classes share. It will also be responsible for
+the initialization and conversion (JSON <=> Python), mainly called
+serialization and deserialization, and will
+have other methods that will be explained later.
 """
 
-# This will allow us to directly use the datetime class and create datetime objects (dates and times)
-from datetime import datetime
-
-# Th3is will allow us to work with with Universally Unique Identifiers that are useful for generating identifiers (create unique IDs)
 from uuid import uuid4
+from datetime import datetime
+from models import storage
+import uuid
+import json
+import sys
+import os.path
 
-# This will allow us to have acces to our model "models" :contains definitions for classes, functions ... that we want to use
-import models
 
 class BaseModel:
+    """
+    The BaseModel class serves as the backbone of our project,
+    encapsulating common attributes and methods shared by other classes.
+    It is responsible for initialization, serialization, deserialization,
+    and other functionalities to be explained later.
+    """
 
-
-    # This is the constructor method for the BaseModel class, it initializes the instance variables and won't return any value
     def __init__(self, *args, **kwargs) -> None:
+        """
+        This is the constructor method for the BaseModel class,
+        it initializes the instance variables and won't return any value.
+        """
+        pass
 
-    # This method to return a string representation of the instance: should print: [<class name>] (<self.id>) <self.__dict__>
     def __str__(self) -> str:
+        """
+        This method returns a string representation of the instance:
+        should print: [<class name>] (<self.id>) <self.__dict__>
+        """
+        pass
 
-    # This method updates the public instance attribute updated_at with the current datetime and save the instance
     def save(self) -> None:
+        """
+        This method updates the public instance attribute updated_at
+        with the current datetime and saves the instance.
+        """
+        pass
 
-    # This method returns a dictionary containing all keys/values of __dict__ of the instance
     def to_dict(self) -> dict:
-
+        """
+        This method returns a dictionary containing all keys/values
+        of __dict__ of the instance.
+        """
+        pass
