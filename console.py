ec745e09e49373fd72677d1bc7098d9567fa96e5
TachiYassine <tachi.yassine.3@gmail.com>
2024-05-18 20:34:27 -0700

    Command-Line Interface for AirBnB instance management

--- console.py
+++ console.py
@@ -1,71 +1,63 @@
 #!/usr/bin/python3
+"""
+Module console.py - a command-line interpreter for HBNB project
+"""
+
 import cmd
+from datetime import datetime
 import json
 import shlex
-from models.base_model import BaseModel
-from models import storage
-from models.engine.file_storage import FileStorage
-from datetime import datetime
 
-"""
-Module consule.py a cmd console
-"""
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    ''' a command interpreter class '''
-    prompt = "(hbnb)"
+    """
+    Command interpreter class for the HBNB project.
+    """
+
+    prompt = "(hbnb) "
 
     All_class_dict = {
-        "BaseModel": BaseModel
+        "BaseModel": BaseModel,
+        "User": User,
+        "Place": Place,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
     }
 
     def do_EOF(self, arg):
-        '''Quit command CTRL+D to exit the program'''
+        """
+        Quit command. Allows the user to exit the program using CTRL+D.
+        """
         print("")
         return True
 
     def do_quit(self, arg):
-        ''' Quit command to exit the program '''
+        """
+        Quit command. Allows the user to exit the program by typing 'quit'.
+        """
         return True
 
     def do_nothing(self, arg):
-        ''' does nothing '''
+        """
+        Dummy command that does nothing.
+        """
         pass
 
     def emptyline(self):
-        ''' an empty line + ENTER shouldn’t execute anything '''
+        """
+        Called when an empty line + ENTER is entered. It shouldn't execute anything.
+        """
         pass
 
-    def do_create(self, args):
-        ''' Creates a new instance BaseModel, saves prints its id '''
-        if args == "":
-            print("** class name missing **")
-            return
-        arg = shlex.split(args)
-        if arg[0] not in HBNBCommand.All_class_dict:
-            print("** class doesn't exist **")
-            return
-        new = HBNBCommand.All_class_dict[arg[0]]()
-        new.save()
-        print(new.id)
-
-    def do_show(self, args):
-        ''' Prints the string repr of an instances class name and id '''
-        arg = shlex.split(args)
-        if len(arg) == 0:
-            print("** class name missing **")
-            return
-        if arg[0] not in HBNBCommand.All_class_dict:
-            print("** class doesn't exist **")
-            return
-        if len(arg) < 2:
-            print("** instance id missing **")
-            return
-        storage.reload()
-        obj = storage.all()
-        obj_key = arg[0] + "." + arg[1]
-        if obj_key in obj:
-            print(str(obj[obj_key]))
-        else:
-            print("** no instance found **")
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
