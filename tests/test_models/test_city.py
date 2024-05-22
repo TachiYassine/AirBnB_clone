7f5c33152ed5a19dd2d77fb43fa614e825a7fd76
Imane-Bjr <boujirimane1@gmail.com>
2024-05-22 02:04:32 +0100

    fixing tasks

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,57 +1,36 @@
 #!/usr/bin/python3
-""" This py file will make all the unit tests
-for our class City and its objects"""
-
+""" Unit test for City """
 import unittest
 from models.city import City
 from datetime import datetime
 
 
-class MyCityTest(unittest.TestCase):
+class CityTestCase(unittest.TestCase):
+    """ Class for City test """
 
     def setUp(self):
-        # In order to test our class we need to Initialize a City instance
+        """ Set up a City instance for testing """
         self.city = City()
 
-    def test_for_attributes_existence(self):
-        """ Test the existence of our attributes """
+    def tearDown(self):
+        """ Clean up after each test """
+        del self.city
+
+    def test_existence(self):
+        """ Test existence of attributes """
         self.assertTrue(hasattr(self.city, "id"))
         self.assertTrue(hasattr(self.city, "created_at"))
         self.assertTrue(hasattr(self.city, "updated_at"))
         self.assertTrue(hasattr(self.city, "state_id"))
         self.assertTrue(hasattr(self.city, "name"))
 
-    def test_for_attributes_types(self):
-        """ Test the types of our attributes """
+    def test_types(self):
+        """ Test types of attributes """
         self.assertIsInstance(self.city.id, str)
         self.assertIsInstance(self.city.created_at, datetime)
         self.assertIsInstance(self.city.updated_at, datetime)
         self.assertIsInstance(self.city.state_id, str)
         self.assertIsInstance(self.city.name, str)
-
-    def test_for_to_dict_method(self):
-        """ Test the to_dict() method """
-        city_dict = self.city.to_dict()
-        self.assertEqual(city_dict['__class__'], 'City')
-        self.assertEqual(city_dict['id'], self.city.id)
-        self.assertEqual(city_dict['created_at'], self.city.created_at.isoformat())
-        self.assertEqual(city_dict['updated_at'], self.city.updated_at.isoformat())
-        self.assertEqual(city_dict['state_id'], self.city.state_id)
-        self.assertEqual(city_dict['name'], self.city.name)
-
-    def test_my_city(self):
-        """ Test existence and types of attributes in a single method """
-        new = City()
-        self.assertTrue(hasattr(new, "id"))
-        self.assertTrue(hasattr(new, "created_at"))
-        self.assertTrue(hasattr(new, "updated_at"))
-        self.assertTrue(hasattr(new, "state_id"))
-        self.assertTrue(hasattr(new, "name"))
-        self.assertIsInstance(new.id, str)
-        self.assertIsInstance(new.created_at, datetime)
-        self.assertIsInstance(new.updated_at, datetime)
-        self.assertIsInstance(new.state_id, str)
-        self.assertIsInstance(new.name, str)
 
 
 if __name__ == '__main__':
